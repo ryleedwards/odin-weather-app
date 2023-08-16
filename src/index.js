7bc947a621754cf34c56e40efc0f3de728415946
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-16 11:11:52 -0500

    Move primary api calls from apiFunc here

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
+import { displayCurrentWeather } from './dom';
 import './style.css';
 
 const inputLocation = document.querySelector(
   'form.form-location>div>#location'
 );
+inputLocation.value = 'Austin, TX';
+const locationWarning = document.querySelector('.location-validation-warning');
+
+let units = 'metric';
 
 const btnSubmitLocation = document.querySelector('.btn.submit-location');
 btnSubmitLocation.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log(inputLocation.value);
+  const location = inputLocation.value;
+  getCurrentWeather(location);
 });
+
+async function getCurrentWeather(location) {
+  try {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY} &q=${location}&aqi=no`;
+    const response = await fetch(url, { mode: 'cors' });
+    if (response.status === 200) {
+      let data = await response.json();
+      displayCurrentWeather(data, units);
+    }
+    if (response.status === 400) {
+      throw new Error('Invalid Location');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
