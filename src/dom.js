bd9e58be15d0ba1ac97a08cd95dc930fa44c2e08
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-16 11:19:18 -0500

    Remove region element

--- src/dom.js
+++ src/dom.js
@@ -2,10 +2,13 @@ const currentTemperature = document.querySelector('p.current-temperature');
 const city = document.querySelector('p.city');
 
 function displayCurrentWeather(jsonWeather, units) {
+  console.log(jsonWeather);
+  //temperature
   if (units == 'imperial')
     currentTemperature.textContent = `${jsonWeather.current.temp_f}°F`;
   if (units == 'metric')
     currentTemperature.textContent = `${jsonWeather.current.temp_c}°C`;
+  //city
   city.textContent = jsonWeather.location.name;
 }
 
