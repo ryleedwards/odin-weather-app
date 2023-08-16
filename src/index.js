d5bc276b932836de3db6b5e19eeab0fdcc624519
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-15 23:34:59 -0500

    Revert styling to basic to build app functionality first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+const inputLocation = document.querySelector(
+  'form.form-location>div>#location'
+);
+
+const btnSubmitLocation = document.querySelector('.btn.submit-location');
+btnSubmitLocation.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(inputLocation.value);
+});
