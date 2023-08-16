90a7a72e0e4415c1f22fad4925c4c9d625cc2169
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-16 11:10:15 -0500

    Add displayCurrentWeather func

--- src/dom.js
+++ src/dom.js
@@ -1,1 +1,12 @@
-export {};
+const currentTemperature = document.querySelector('p.current-temperature');
+const city = document.querySelector('p.city');
+
+function displayCurrentWeather(jsonWeather, units) {
+  if (units == 'imperial')
+    currentTemperature.textContent = `${jsonWeather.current.temp_f}°F`;
+  if (units == 'metric')
+    currentTemperature.textContent = `${jsonWeather.current.temp_c}°C`;
+  city.textContent = jsonWeather.location.name;
+}
+
+export { displayCurrentWeather };
